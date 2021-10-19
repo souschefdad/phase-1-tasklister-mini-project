d2994799f9a71ee58afc71411ea66cfb31e6c698
Pierce <piercedavidraphael@gmail.com>
2021-10-19 12:46:26 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+      e.preventDefault()
+      taskHandler(e.target.new_task_description.value)
+  })
 });
+
+
+function taskHandler (todo) {
+    let li = document.createElement("li");
+    li.textContent = todo
+    document.querySelector("#tasks").appendChild(li)
+}
+
+
+
